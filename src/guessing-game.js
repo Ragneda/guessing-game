e68d5753c36d9148e666ae92b46ca6bf2fdfae60
Alena Rebkavets <archirebkavets@gmail.com>
2023-02-20 23:02:56 +0100

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.gNumer = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.gNumer = Math.round((this.max + this.min) / 2);
+    return this.gNumer;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.gNumer;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.gNumer;
+  }
 }
 
 module.exports = GuessingGame;
